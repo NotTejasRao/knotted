36751fdd8ca0a1252a73db2ff4374fa14b2bd7c8
Tejas Rao <nottejasrao@gmail.com>
2020-06-12 14:48:42 -0400

    Create Goal model

---
+++ backend/models/Goal.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const Habit = require('./Habit');
+
+const GoalSchema = new mongoose.Schema({
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+    },
+    habits: [Habit]
+});
+
+module.exports = Goal = mongoose.mongo.model('user', GoalSchema);
