54e034d9d571b53097e7ab757cc07dbe1180dc9e
Tejas Rao <nottejasrao@gmail.com>
2020-06-12 20:55:00 -0400

    Update route definitions

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require("express");
+const connectDB = require("./config/db");
 
 // Create Application
 const app = express();
@@ -11,12 +11,13 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 // Configure Routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/goals", require("./routes/api/goals"));
+app.use("/api/habits", require("./routes/api/habits"));
 
 // Start Web Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}.`);
+  console.log(`Server started on port ${PORT}.`);
 });
