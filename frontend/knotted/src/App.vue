6529dca89c8e03e58331521d4e02dab01135c44b
Tejas Rao <nottejasrao@gmail.com>
2020-06-13 15:31:53 -0400

    Add vuetify

---
+++ frontend/knotted/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-content>
+      <Knotted />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Knotted from "./components/Knotted";
+
+export default {
+  name: "App",
+
+  components: {
+    Knotted,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ frontend/knotted/src/components/Knotted.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Knotted</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
