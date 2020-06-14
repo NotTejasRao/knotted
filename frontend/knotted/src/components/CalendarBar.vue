18ec76992e805d687ebfa2837a6343bbec34b46a
Tejas Rao <nottejasrao@gmail.com>
2020-06-14 17:45:35 -0400

    Create CalendarBar

---
+++ frontend/knotted/src/components/CalendarBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-toolbar dense short color="#2B2D42" dark>
+    <v-btn icon>
+      <v-icon>mdi-chevron-left</v-icon>
+    </v-btn>
+
+    <v-spacer></v-spacer>
+    <v-toolbar-title>May 6, 2020</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn icon>
+      <v-icon>mdi-chevron-right</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
