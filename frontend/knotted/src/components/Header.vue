10b9f3edeac7acf13cfaeb9dfaf4cec96aab7d1b
Tejas Rao <nottejasrao@gmail.com>
2020-06-14 00:36:33 -0400

    Add Header

---
+++ frontend/knotted/src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="header">
+    <v-app-bar dark>
+      <v-spacer></v-spacer>
+      <v-toolbar-title center>Knotted</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
