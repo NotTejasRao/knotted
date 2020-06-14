cab5c575a17ef15e2f35ee286ab76ff862b2949e
Tejas Rao <nottejasrao@gmail.com>
2020-06-14 17:28:40 -0400

    Create Habit template

---
+++ frontend/knotted/src/components/Habit.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card class="ma-4" to="habits">
+    <v-container>
+      <v-row>
+        <v-col cols="10">
+          <v-card-title>Morning Routine</v-card-title>
+        </v-col>
+        <v-col cols="2">
+          <v-checkbox></v-checkbox>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+
+<script>
+export default {};
+</script>
