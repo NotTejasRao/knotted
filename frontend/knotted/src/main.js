127dd7d2a2b3b2d79c6cb5e25aac807a5f48ae29
Tejas Rao <nottejasrao@gmail.com>
2020-06-14 00:35:58 -0400

    Add router

--- frontend/knotted/src/main.js
+++ frontend/knotted/src/main.js
@@ -2,10 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import vuetify from "./plugins/vuetify";
+import router from "./router";
 
 Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
-  render: h => h(App)
+  router,
+  render: (h) => h(App),
 }).$mount("#app");
