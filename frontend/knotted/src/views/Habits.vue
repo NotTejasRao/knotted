012df16514d57150817fb675daf066ff4990dee6
Tejas Rao <nottejasrao@gmail.com>
2020-06-18 14:30:02 -0400

    Make Habits page dynamic

--- frontend/knotted/src/views/Habits.vue
+++ frontend/knotted/src/views/Habits.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="habits">
-    <Header />
+    <v-content>
+      <Header />
+
+      <Habit
+        v-for="habit in habits"
+        :key="habit.id"
+        :name="habit.name"
+        :completed="habit.completed"
+        @clicked="habitClicked(id)"
+      ></Habit>
+    </v-content>
+
+    <v-btn color="indigo" bottom right fixed dark fab class="mb-12">
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
+
     <BottomNavbar />
   </div>
 </template>
@@ -9,11 +24,24 @@
 import Header from "@/components/Header.vue";
 import BottomNavbar from "@/components/BottomNavbar.vue";
 
+import Habit from "@/components/Habit.vue";
+
 export default {
   name: "Habits",
   components: {
     Header,
     BottomNavbar,
+    Habit
   },
+  data() {
+    return {
+      habits: []
+    };
+  },
+  methods: {
+    habitClicked(id) {
+      // open goal list
+    }
+  }
 };
 </script>
