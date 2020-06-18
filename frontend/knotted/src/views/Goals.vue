5833e060397efc710d615325520eb448554129e0
Tejas Rao <nottejasrao@gmail.com>
2020-06-18 14:29:55 -0400

    Make Goals page dynamic

--- frontend/knotted/src/views/Goals.vue
+++ frontend/knotted/src/views/Goals.vue
@@ -2,19 +2,51 @@
   <div class="goals">
     <Header />
 
+    <v-content>
+      <CalendarBar />
+
+      <Goal
+        v-for="goal in goals"
+        :key="goal.id"
+        :name="goal.name"
+        :completed="goal.completed"
+        :total="goal.total"
+        @clicked="goalClicked(id)"
+      ></Goal>
+    </v-content>
+
+    <v-btn color="indigo" bottom right fixed dark fab class="mb-12">
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
+
     <BottomNavbar />
   </div>
 </template>
 
 <script>
 import Header from "@/components/Header.vue";
+import CalendarBar from "@/components/CalendarBar.vue";
 import BottomNavbar from "@/components/BottomNavbar.vue";
+
+import Goal from "@/components/Goal.vue";
 
 export default {
   name: "Goals",
   components: {
     Header,
+    CalendarBar,
     BottomNavbar,
+    Goal
   },
+  data() {
+    return {
+      goals: []
+    };
+  },
+  methods: {
+    goalClicked(id) {
+      // open habits list
+    }
+  }
 };
 </script>
