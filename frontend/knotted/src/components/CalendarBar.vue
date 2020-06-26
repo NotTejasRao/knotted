0092f0a99b8846899154b31ed4447db7d0308ceb
Tejas Rao <nottejasrao@gmail.com>
2020-06-25 23:35:13 -0400

    Create calendar state

--- frontend/knotted/src/components/CalendarBar.vue
+++ frontend/knotted/src/components/CalendarBar.vue
@@ -1,22 +1,26 @@
 <template>
   <v-toolbar dense short color="#2B2D42" dark>
-    <v-btn icon>
+    <v-btn icon @click.native="previousDate()">
       <v-icon>mdi-chevron-left</v-icon>
     </v-btn>
 
     <v-spacer></v-spacer>
-    <v-toolbar-title>May 6, 2020</v-toolbar-title>
+    <v-toolbar-title>{{ getDate().toLocaleDateString() }}</v-toolbar-title>
 
     <v-spacer></v-spacer>
 
-    <v-btn icon>
+    <v-btn icon @click.native="nextDate()">
       <v-icon>mdi-chevron-right</v-icon>
     </v-btn>
   </v-toolbar>
 </template>
 
 <script>
-export default {};
+import { mapGetters, mapActions } from "vuex";
+export default {
+  computed: mapGetters(["getDate"]),
+  methods: mapActions(["nextDate", "previousDate"])
+};
 </script>
 
 <style scoped></style>
